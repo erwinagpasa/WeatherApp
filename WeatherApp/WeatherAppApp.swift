857b3553971925230a98dffa3740f90347945e4a
erwinagpasa <rootaccess000@gmail.com>
2023-05-13 16:55:02 +0300

    initial commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Developer on 13/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
